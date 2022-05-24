23dd038796261409a8c60489dac64b9f870456f7
Aurel1701 <gantzfan96@gmail.com>
2022-05-24 14:46:50 +0200

    add cdn bootstrap

--- index.php
+++ index.php
@@ -12,12 +12,18 @@ include __DIR__ . "/data.php";
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
     <link rel="stylesheet" href="./assets/style.css">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
 </head>
 <body>
 <header class="contain">
     <div class="logo">
       <img class="img-logo" src="./assets/logo.png" alt="" />
     </div>
-  </header>
+</header>
+
+<!-- creo nel main la struttara per visulizzare le card -->
+<main>
+
+</main>
 </body>
 </html>

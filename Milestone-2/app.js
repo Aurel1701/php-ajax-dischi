daf8b03b6ac44144306d37cc26e2c1aed49b4fd7
Aurel1701 <gantzfan96@gmail.com>
2022-05-24 16:20:06 +0200

    edit axios

--- Milestone-2/app.js
+++ Milestone-2/app.js
@@ -4,7 +4,7 @@ new Vue({
         discs: [],
     },
     created() {
-        axios.get('http://localhost:8888/intro-PHP/php-ajax-dischi/Milestone-2/vue.html').then((response) => {
+        axios.get('http://localhost:8888/intro-PHP/php-ajax-dischi/Milestone-2/discs.php').then((response) => {
             this.discs = response.data;
         })
     }
